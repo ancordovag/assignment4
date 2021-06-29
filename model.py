d1989abfb4f21ee8e62eea6bc8283c09e2b4403c
Andrés Córdova <ancordova25@users.noreply.github.com>
2021-06-29 21:53:37 +0200

    debugging 3

--- model.py
+++ model.py
@@ -14,7 +14,7 @@ class SentEncoder(nn.Module):
         conv_dim = configs["conv_dim"]
         self.embed = nn.Embedding.from_pretrained(pretrained_emb)
         self.conv = nn.Conv1d(50, conv_dim,(2))
-        self.relu = nn.ReLU
+        self.relu = nn.ReLU()
 
 
     def forward(self, sent):
